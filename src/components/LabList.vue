68ea82cacfc5513b020539cdac869219de8fdec5
DavidST <d.strucken@mat-solutions.de>
2023-12-15 16:07:31 +0100

    printbutton

--- src/components/LabList.vue
+++ src/components/LabList.vue
@@ -2,7 +2,7 @@
     <div class="modal">
       <div class="modal-content flex flex-col items-center">
         <div class="header flex">
-            <button type="button" @click="" class="mt-4 text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">New</button>
+            <button type="button" @click="handleNewButtonClick" class="mt-4 text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">New</button>
             <button type="button" @click="" class="mt-4  text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Open</button>
         </div>
 
@@ -28,7 +28,7 @@
               {{ item.Paramter }}
             </th>
             <td class="px-6 py-4 dark:text-black">
-              <input v-model="item.Value" type="text" text-center/>
+              <input v-model="item.Value" type="text"/>
             </td>
             <td class="px-6 py-4 dark:text-black">
               {{ item.Unit }}
@@ -41,7 +41,7 @@
       </table>
 
         <div class="flex">
-        <button type="button" @click="" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Print</button>
+        <PrintButton @print="handlePrintButtonClick" />
         <button type="button" @click="" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Save</button>
       </div>
      </div>
@@ -49,6 +49,8 @@
   </template>
   
   <script>
+  import PrintButton from './PrintButton.vue';
+
   export default {
     props: {
       show: Boolean,
@@ -57,21 +59,43 @@
     data() {
       return {
         currentValue: '',
-        data : [
-            { Paramter: 'Probe Date/Time', Value: 'Placeholder'},
-            { Paramter: 'Tester', Value: 'Placeholder'},
-            { Paramter: 'Product', Value: 'Placeholder'},
-            { Paramter: 'Batch number', Value: 'Placeholder'}
-            ],
-        data2 : [
-            { Paramter: 'Required Parameter A', Value: 'Placeholder', Unit: 'Unit'},
-            { Paramter: 'Required Parameter B', Value: 'Placeholder', Unit: 'Unit'},
-            { Paramter: 'Optional Parameter A', Value: 'Placeholder', Unit: 'Unit'},
-            { Paramter: 'Optional Parameter A', Value: 'Placeholder', Unit: 'Unit'}
-          ],
-      }}      
-      }
+        data: [
+          { Paramter: 'Probe Date/Time', Value: 'Placeholder' },
+          { Paramter: 'Tester', Value: 'Placeholder' },
+          { Paramter: 'Product', Value: 'Placeholder' },
+          { Paramter: 'Batch number', Value: 'Placeholder' },
+        ],
+        data2: [
+          { Paramter: 'Required Parameter A', Value: 'Placeholder', Unit: 'Unit' },
+          { Paramter: 'Required Parameter B', Value: 'Placeholder', Unit: 'Unit' },
+          { Paramter: 'Optional Parameter A', Value: 'Placeholder', Unit: 'Unit' },
+          { Paramter: 'Optional Parameter A', Value: 'Placeholder', Unit: 'Unit' },
+        ],
+      };
+    },
+  
+    methods: {
+      handleNewButtonClick() {
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleString(); // You can customize the date format as needed
+  
+        // Find the "Probe Date/Time" item in the data array and update its value
+        const probeDateTimeItem = this.data.find(item => item.Paramter === 'Probe Date/Time');
+        if (probeDateTimeItem) {
+          probeDateTimeItem.Value = formattedDate;
+        }
+      },
+  
+      // Add other methods as needed
+    },
+
+    components: {
+      PrintButton,
+    },
+
+  };
   </script>
+  
 
 
 <style>
@@ -88,6 +112,10 @@
   }
   .btn:hover {
     background-color: #1a202c; /* Ändere die Farbe nach Bedarf */
+  }
+
+  input {
+    text-align: center;
   }
 
 </style>
