318b20c6b1b8938b04fa988df3a55708afbf0077
DavidST <d.strucken@mat-solutions.de>
2024-01-04 17:54:24 +0100

    all inputs are done

---
+++ src/components/ArticleNumberInput.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="tester-input" ref="testerInput">
+      <div class="input-container">
+        <input
+          type="text"
+          v-model="suggest"
+          @input="handleInput"
+          @focus="handleFocus"
+        />
+        <div v-if="showSuggestions" class="suggestions-container" @click.stop="">
+          <ul>
+            <li v-for="suggest in suggestions" @click="selectSuggestion(suggest)">
+              {{ suggest }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    const { InfluxDB } = require('@influxdata/influxdb-client');
+    const url = "http://localhost:8086"
+    const token = "Qc6s7RKI7ZnQpB5ZdesJzEmgd46XLGRmcXv5RJRbhTUc758Ma8g-LQv6_A2p125BZohkhbYnEhVtpeOHJ-BqTw=="
+    const org = "MAT"
+    const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
+  
+  export default {
+    data() {
+      return {
+        suggest: "",
+        suggestions: [],
+        allArticleNumbers: [],
+        showSuggestions: false
+      };
+    },
+  
+    created() {
+      // Laden Sie die Daten beim Erstellen der Komponente
+      this.fetchNames();
+    },
+  
+    methods: {
+      async fetchNames() {
+        const fluxQuery = 'from(bucket: "LabData") |> range(start: 0, stop: now()) |> filter(fn: (r) => r["_measurement"] == "HeaderData") |> group(columns: ["_field"])   |> sort(columns: ["_time"], desc: true) |> limit(n: 5)';
+        const result = await queryApi.collectRows(fluxQuery);
+        this.allArticleNumbers = [...new Set(result.map(row => row.Article_No))];
+        console.log(this.allArticleNumbers);
+        console.log('hi');
+      },
+  
+      filterNames() {
+        if (this.suggest.length === 0) {
+          this.suggestions = this.allArticleNumbers;
+        } else {
+          this.suggestions = this.allArticleNumbers.filter(name =>
+            name.toLowerCase().includes(this.suggest.toLowerCase())
+          );
+        }
+      },
+  
+      selectSuggestion(suggest) {
+        this.suggest = suggest;
+        this.showSuggestions = false;
+      },
+  
+      handleInput() {
+        this.filterNames();
+      },
+  
+      handleFocus() {
+        this.showSuggestions = true;
+        this.filterNames();
+        // Hinzugefügt: Event Listener für das Schließen des Menüs bei Klick auf die Seite
+        document.addEventListener("click", this.closeSuggestions);
+      },
+  
+      closeSuggestions(event) {
+        const testerInput = this.$refs.testerInput;
+        if (!testerInput.contains(event.target)) {
+          this.showSuggestions = false;
+          document.removeEventListener("click", this.closeSuggestions);
+        }
+      }
+    },
+  
+    watch: {
+      suggest() {
+        this.filterNames();
+      }
+    }
+  };
+  </script>
+  
+    
+    <style scoped>
+    .tester-input {
+      position: relative;
+    }
+    
+    .input-container {
+      position: relative;
+    }
+    
+    .suggestions-container {
+      position: absolute;
+      top: 100%;
+      left: 0;
+      width: 100%;
+      max-height: 150px; /* Set your desired max height */
+      overflow-y: auto;
+      background-color: #fff;
+      border: 1px solid #ccc;
+      border-top: none;
+      z-index: 1;
+    }
+    
+    ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+    }
+    
+    li {
+      padding: 8px;
+      cursor: pointer;
+    }
+    
+    li:hover {
+      background-color: #f1f1f1;
+    }
+    </style>
--- src/components/LabList.vue
+++ src/components/LabList.vue
@@ -7,50 +7,65 @@
           <button type="button" @click="" class="hidden-print mt-4  text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Open</button>
         </div>
 
-    
-        <table  class="mx-auto my-4 border-separate border-spacing-2 border border-slate-500 text-sm text-left dark:bg-gray-100 text-gray-500 dark:text-gray-100 mt-100">
-          <tbody id="labUIdata">
-            <tr class="bg-white border-b dark:bg-gray-200 dark:border-gray-300" v-for="(item, index) in this.data" :key="index">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                {{ item.Paramter }}
-              </th>
-              <td class="px-6 py-4 dark:text-black">
-                <template v-if="item.Paramter === 'Tester'">
-                  <TesterInput v-model="testerName"/>
-                </template>
-                <template v-else>
-                  <input v-model="item.Value" type="text" />
-                </template>
-              </td>
+          <template v-if="isNewButtonClicked || isOpenButtonClicked">
+      
+            <table  class="mx-auto my-4 border-separate border-spacing-2 border border-slate-500 text-sm text-left dark:bg-gray-100 text-gray-500 dark:text-gray-100 mt-100">
+              <tbody id="labUIdata">
+                <tr class="bg-white border-b dark:bg-gray-200 dark:border-gray-300" v-for="(item, index) in this.data" :key="index">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                    {{ item.Paramter }}
+                  </th>
+                  <td class="px-6 py-4 dark:text-black">
+                    <template v-if="item.Paramter === 'Tester'">
+                      <TesterInput v-model="testerName"/>
+                    </template>
+                    <template v-else-if="item.Paramter === 'Article'">
+                      <ArticleInput v-model="testerName"/>
+                    </template>
+                    <template v-else-if="item.Paramter === 'Batch number'">
+                      <BatchInput v-model="testerName"/>
+                    </template>
+                    <template v-else-if="item.Paramter === 'Order number'">
+                      <OrderInput v-model="testerName"/>
+                    </template>
+                    <template v-else-if="item.Paramter === 'Article number'">
+                      <ArticleNumberInput v-model="testerName"/>
+                    </template>
+                    <template v-else>
+                      <input v-model="item.Value" type="text" />
+                    </template>
+                  </td>
 
-            </tr>
-          </tbody>    
-        </table>
+                </tr>
+              </tbody>    
+            </table>
 
-        <table class="mx-auto my-4 border-separate border-spacing-2 border border-slate-500 text-sm text-left dark:bg-gray-100 text-gray-500 dark:text-gray-100 mt-100">
-        <tbody>
-          <tr class="bg-white border-b dark:bg-gray-200 dark:border-gray-300" v-for="(item, index) in this.data2" :key="index">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-              {{ item.Paramter }}
-            </th>
-            <td class="px-6 py-4 dark:text-black">
-              <input v-model="item.Value" type="text"/>
-            </td>
-            <td class="px-6 py-4 dark:text-black">
-              {{ item.Unit }}
-            </td>
-            <td class="px-6 py-4 dark:text-black">
-              <button type="button" @click="handleSetButtonClick(item)" class="btn hidden-print">Set</button>
-            </td>
-          </tr>
-        </tbody>    
-      </table>
+            <table class="mx-auto my-4 border-separate border-spacing-2 border border-slate-500 text-sm text-left dark:bg-gray-100 text-gray-500 dark:text-gray-100 mt-100">
+              <tbody>
+                <tr class="bg-white border-b dark:bg-gray-200 dark:border-gray-300" v-for="(item, index) in this.data2" :key="index">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                    {{ item.Paramter }}
+                  </th>
+                  <td class="px-6 py-4 dark:text-black">
+                    <input v-model="item.Value" type="text"/>
+                  </td>
+                  <td class="px-6 py-4 dark:text-black">
+                    {{ item.Unit }}
+                  </td>
+                  <td class="px-6 py-4 dark:text-black">
+                    <button type="button" @click="handleSetButtonClick(item)" class="btn hidden-print">Set</button>
+                  </td>
+                </tr>
+              </tbody>    
+            </table>
 
-        <div class="flex">
-          
-        <PrintButton/>
-        <button type="button" @click="" class="hidden-print text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Save</button>
-      </div>
+            <div class="flex">
+              <PrintButton/>
+              <button type="button" @click="" class="hidden-print text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2 mr-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Save</button>
+            </div>
+
+          </template>
+
      </div>
     </div>
   </template>
@@ -58,7 +73,11 @@
   <script>
   import NewButton from './NewButton.vue';
   import PrintButton from './PrintButton.vue';
+  import ArticleInput from './ArticleInput.vue';
   import TesterInput from './TesterInput.vue';
+  import BatchInput from './BatchInput.vue';
+  import OrderInput from './OrderInput.vue';
+  import ArticleNumberInput from './ArticleNumberInput.vue';
 
   export default {
     props: {
@@ -67,6 +86,8 @@
   
     data() {
       return {
+        isNewButtonClicked: false,
+        isOpenButtonClicked: false,
         sampleNumber: '',
         currentValue: '',
         testerName: '',
@@ -91,8 +112,16 @@
     },
   
     methods: {
+
+      handleOpenButtonClick() {
+        this.isNewButtonClicked = false;
+        this.isNewButtonClicked = true;
+      },
+
+
       // Function for clicking button "New"
       handleNewButtonClick(result) {
+        this.isNewButtonClicked = true;
         const currentDate = new Date();
         const formattedDate = currentDate.toLocaleString(); // You can customize the date format as needed
   
@@ -115,7 +144,11 @@
     components: {
       NewButton,
       PrintButton,
-      TesterInput
+      TesterInput,
+      ArticleInput,
+      BatchInput,
+      OrderInput,
+      ArticleNumberInput
     },
 
   };
